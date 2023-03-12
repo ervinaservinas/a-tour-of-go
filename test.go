11bd066991c1116f1052044d8f9fc24b6257cff6
Ervinas <84942742+ervinaservinas@users.noreply.github.com>
2023-03-12 21:02:17 +0200

    time commit

--- test.go
+++ test.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, 世界, my test with GO language")
+	fmt.Println("Welcome to the playground!")
+	fmt.Println("The time is", time.Now())
 }
